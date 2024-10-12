0d4eb4698ff21977f31afacfb429ee30deb8b9a8
Edward <edwardduong71311@gmail.com>
2024-10-11 21:04:03 -0500

    fix(AI-1): remove aws keys

--- main.tf
+++ main.tf
@@ -2,15 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.45.0"
+      version = "5.71.0"
     }
   }
 }
 
 provider "aws" {
   region  = "us-east-1"
-  access_key = "enter_access_key_here"
-  secret_key = "enter_secret_key_here"
 }
 
 resource "aws_ecr_repository" "ai_ecr_repo" {
