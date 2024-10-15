0480cdc85e429f849f3cd002632b0214ecad34a3
Edward <edwardduong71311@gmail.com>
2024-10-14 20:28:56 -0500

    fix(AI-1-2): resolve conflicts

--- modules/ecs_task/main.tf
+++ modules/ecs_task/main.tf
@@ -1,25 +1,33 @@
-# resource "aws_ecs_task_definition" "ai_task" {
-#   family                   = "ai-task"
-#   container_definitions    = <<DEFINITION
-#   [
-#     {
-#       "name": "ai-task",
-#       "image": "${aws_ecr_repository.ai_ecr_repo.repository_url}",
-#       "essential": true,
-#       "portMappings": [
-#         {
-#           "containerPort": 5000,
-#           "hostPort": 5000
-#         }
-#       ],
-#       "memory": 512,
-#       "cpu": 256
-#     }
-#   ]
-#   DEFINITION
-#   requires_compatibilities = ["FARGATE"]
-#   network_mode             = "awsvpc"
-#   memory                   = 512
-#   cpu                      = 256
-#   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
-# }
+module "ecr" {
+  source = "../ecr"
+}
+
+module "iam" {
+  source = "../iam"
+}
+
+resource "aws_ecs_task_definition" "ai_task" {
+  family                   = "ai-task"
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "ai-task",
+      "image": "${module.ecr.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  cpu                      = 256
+  execution_role_arn       = "${module.iam.arn}"
+}
